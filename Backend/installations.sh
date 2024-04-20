30333fdd6c1d7b14ba0a910cfc7dbffc8eba33ed
ellenwei <ellenwei@PersonalMicrosoftSoftware.ucla.edu>
2024-04-20 16:38:15 -0700

    add model files, no tflite file

--- Backend/installations.sh
+++ Backend/installations.sh
@@ -4,3 +4,10 @@ python -m pip install flask
 python -m pip install flask_cors
 python -m pip install requests
 pip install -q -U google-generativeai
+
+
+# for the tflite model
+pip install numpy
+pip install tensorflow
+pip install opencv-python
+pip install pathlib
