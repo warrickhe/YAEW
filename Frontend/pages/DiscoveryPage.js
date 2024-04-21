c8fee44ced992c56e99e39fea8bb8fa8861e4c15
Yifan Tang <yifant@g.ucla.edu>
2024-04-21 05:34:04 -0700

    Added loading and you found a ___ page

--- Frontend/pages/DiscoveryPage.js
+++ Frontend/pages/DiscoveryPage.js
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Image, StyleSheet, Button } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 
 const BACKEND_URL = 'http://192.168.1.100:7272';
 
 // Define the data with questions, correct and wrong answers
-const data = {
-  name: 'monkey',
-  points: 3,
-  image_url:
-    'https://static.boredpanda.com/blog/wp-content/uploads/2018/04/5acb63d83493f__700-png.jpg',
-  description: "It's a cat, a cute cat, a funny cat, a good cat",
-};
+// const data = {
+//   name: 'monkey',
+//   points: 3,
+//   image_url:
+//     'https://static.boredpanda.com/blog/wp-content/uploads/2018/04/5acb63d83493f__700-png.jpg',
+//   description: "It's a cat, a cute cat, a funny cat, a good cat",
+// };
 
-export default function DiscoveryPage() {
+export default function DiscoveryPage({ navigation, route }) {
   const [deviceID, setDeviceID] = useState('');
+
+  const data = route.params.data;
 
   useEffect(() => {
     const fetchDeviceID = async () => {
@@ -25,20 +27,29 @@ export default function DiscoveryPage() {
     fetchDeviceID();
   }, []);
 
+  function handleClick() {
+    navigation.navigate('HomePage');
+  }
+
+  function convertBinary(image) {
+    if (image && image.$binary && image.$binary.base64) {
+      return `data:image/png;base64,${image.$binary.base64}`;
+    }
+  }
+
   return (
     <View style={styles.container}>
       {/* White box containing the quiz question */}
       <View style={styles.whiteBox}>
-        <Text style={styles.TitleText}>You Found A {data.name}!</Text>
-        <Text style={styles.PointText}>+{data.points} points!</Text>
-        <Image source={{ uri: data.image_url }} style={styles.image} />
-        <Text style={styles.DescriptionText}>{data.description}</Text>
+        <Text style={styles.TitleText}>You Found A {data.animal}!</Text>
+        <Text style={styles.PointText}>+5 points!</Text>
+        {/* <Image source={{ uri: data.image_url }} style={styles.image} /> */}
+        {/* <Text style={styles.DescriptionText}>{data.description}</Text> */}
         <View style={styles.okButtonContainer}>
-          {/* //TODO:Add OK navigation, back to homepage? */}
           <Button
             title="OK"
             onPress={() => {
-              /* Add button functionality here if needed */
+              handleClick();
             }}
             color="white"
           />
--- Frontend/pages/ImagePickerPage.js
+++ Frontend/pages/ImagePickerPage.js
@@ -1,7 +1,16 @@
 import { useState, useEffect } from 'react';
-import { Text, Button, Image, View, StyleSheet, ActivityIndicator } from 'react-native';
+import {
+  Text,
+  Button,
+  Image,
+  View,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as SecureStore from 'expo-secure-store';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const BACKEND_URL = 'http://192.168.1.100:7272';
 
@@ -65,7 +74,10 @@ export default function ImagePickerExample({ navigation }) {
 
       if (response.ok) {
         console.log('Image uploaded successfully!');
-        navigation.navigate('DiscoveryPage');
+        const resData = await response.json();
+
+        //console.log(resData);
+        navigation.navigate('DiscoveryPage', { data: resData });
       } else {
         console.error('Error uploading image', response.statusText);
       }
@@ -75,84 +87,65 @@ export default function ImagePickerExample({ navigation }) {
   };
 
   return (
-    <>
-      <View style={styles.container}>
-        <Button title="Pick an Image" onPress={pickImage} style={styles.button_container} />
+    <LinearGradient
+      style={styles.container}
+      locations={[0.29, 1]}
+      colors={['#ebf9c9', '#96e6a1']}
+      useAngle={true}
+      angle={168.15}
+    >
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={pickImage} style={styles.button}>
+          <Text style={styles.buttonText}>Pick an Image</Text>
+        </TouchableOpacity>
         {image && (
-          <>
-            <Text>Image Picked!</Text>
-            <Button title="Send Image to Backend" onPress={sendImageToBackend} />
-          </>
+          <TouchableOpacity onPress={sendImageToBackend} style={styles.button}>
+            <Text style={styles.buttonText}>What did I find?</Text>
+          </TouchableOpacity>
         )}
         {sentToBackend && (
-          <>
-            <View>
-              <Text style={styles.LoadingTitleText}>Analyzing...</Text>
-              <ActivityIndicator size="large" />
-            </View>
-          </>
+          <View style={styles.loadingContainer}>
+            <Text style={styles.loadingTitleText}>Analyzing...</Text>
+            <ActivityIndicator size="large" />
+          </View>
         )}
       </View>
-    </>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#CDEBC5',
     justifyContent: 'center',
-  },
-  button_container: {
-    backgroundColor: 'white',
-    padding: 5,
-  },
-
-  titleText: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
-  subtitleText: {
-    fontSize: 16,
-    color: 'green',
-  },
-  itemContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 50,
-    marginTop: 10,
-    width: '100%', // Full-width container
   },
-  image: {
-    width: 50,
-    height: 50,
-    marginRight: 10,
-  },
-  textContainer: {
-    flex: 1, // Take up remaining space in the row
-  },
-  item: {
-    fontSize: 16,
-  },
-  separator: {
-    height: 2,
+  buttonContainer: {
     width: '100%',
-    backgroundColor: '#808080',
-    marginTop: 10,
+    alignItems: 'center',
   },
-  loadingWhiteBox: {
-    backgroundColor: 'white',
-    padding: 20,
-    borderRadius: 10,
-    marginBottom: 20,
-    marginTop: 300,
+  button: {
+    width: '60%',
+    aspectRatio: 1, // Square aspect ratio
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff', // White fill
+    borderWidth: 4, // Blue outline
+    borderColor: '#3f84e5', // Blue outline color
+    borderRadius: 10, // Adjust border radius as needed
+    marginVertical: 10, // Adjust vertical margin as needed
   },
-  LoadingTitleText: {
-    fontSize: 25,
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#3f84e5', // Blue text color
+  },
+  loadingContainer: {
+    alignItems: 'center',
+  },
+  loadingTitleText: {
+    fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
-    marginTop: 20,
-    marginLeft: 75,
   },
 });
